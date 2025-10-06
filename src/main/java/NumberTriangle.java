fc21134e5a32ad913468b2111bbb5e5f3f663a52
Jean J <jeanjung3927@gmail.com>
2025-10-06 17:38:47 -0400

    This fixes #1 Issue

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -116,7 +116,6 @@ public class NumberTriangle {
      * @return the topmost NumberTriangle object in the NumberTriangle structure read from the specified file
      * @throws IOException may naturally occur if an issue reading the file occurs
      */
-    #PR Task2
     public static NumberTriangle loadTriangle(String fname) throws IOException {
         // open the file and get a BufferedReader object whose methods
         // are more convenient to work with when reading the file contents.
